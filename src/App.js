a5e18805bf584e68acde043d19915fa7a40a7b69
Oumaima-bouchniba <bouchnibaoumaima4@gmail.com>
2022-12-30 18:42:56 +0100

    add state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+state = {
+  Person : {
+    fullName: "Eren yeager",
+    bio: " a member of the Survey Corps",
+    imgSrc:" https://villains.fandom.com/wiki/Eren_Yeager",
+    profession: "actor"
+  },
+  show : true
+  };
+
+  handleShowPerson = () => {
+    this.setState({
+      ...this.state,
+      show: !this.state.show
+    });
+}
+
+
+  render() {
+    return (
+      <>
+        {this.setState.show && (
+          <>
+          <h1>{this.state.Person.fullName}</h1>
+            <h1>{this.state.Person.bio}</h1>
+            <img src={this.state.Person.imgSrc} alt="Naruto"></img>
+            <h1>{this.state.Person.profession}</h1>
+            <br></br>
+          </>
+        )}
+        
+        <button onClick={this.handleShowPerson}>click here</button>
+      </>
+    );
+  }
 }
 
 export default App;
+
